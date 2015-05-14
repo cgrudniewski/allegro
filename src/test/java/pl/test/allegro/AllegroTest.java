9b8d2572ccf94cc0576a1e98d8fda2ed35962a62
Czarek Grudniewski <cgrudniewski@gmail.com>
2015-05-14 23:22:18 +0200

    finding item

--- src/test/java/pl/test/allegro/AllegroTest.java
+++ src/test/java/pl/test/allegro/AllegroTest.java
@@ -1,7 +1,9 @@
 package pl.test.allegro;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -16,6 +18,14 @@ public class AllegroTest {
     private WebDriver driver;
     private String baseUrl = "http://allegro.pl";
 
+    @FindBy(id = "main-search-text")
+    private WebElement inputItemName;
+
+    @FindBy(xpath = "//form[@id='main-search']/input[@type='submit']")
+    private WebElement submitButton;
+
+    private final String itemToFind = "gitara";
+
     @BeforeMethod
     public void setUp() throws Exception {
         driver = new FirefoxDriver(FirefoxDriverConf.getFirefoxProfileConfiguration());
@@ -24,10 +34,28 @@ public class AllegroTest {
 
     @Test
     public void test() {
+
+        Page page = new Page(driver);
+
+        page.navigateTo(driver, baseUrl);
     }
 
     @AfterMethod
     public void tearDown() throws Exception {
         driver.quit();
+    }
+
+    private boolean searchItem(final Page page, final String itemToFind) {
+
+        if (page.isElementPresent(inputItemName)) {
+            page.insertStrings(inputItemName, itemToFind);
+            page.clickButtonElement(submitButton);
+
+            System.out.println("1. Wyszukiwanie na allegro przedmiotu zdefiniowanego jako parametr funkcji - OK");
+
+            return true;
+        }
+
+        return false;
     }
 }
