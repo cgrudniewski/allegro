0d0ab638e0227356e4732af21e0e54f1a34b9c9d
Czarek Grudniewski <cgrudniewski@gmail.com>
2015-05-14 22:44:48 +0200

    test methods

--- src/test/java/pl/test/allegro/AllegroTest.java
+++ src/test/java/pl/test/allegro/AllegroTest.java
@@ -1,12 +1,28 @@
 package pl.test.allegro;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 /**
+ *
  * Created by Czarek on 2015-05-14.
  */
 public class AllegroTest {
 
     private WebDriver driver;
     private String baseUrl = "http://allegro.pl";
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+    }
 }
