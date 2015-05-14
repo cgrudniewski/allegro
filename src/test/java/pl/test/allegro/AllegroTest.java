2947a6d17aac3f6129a4cbe9becd8a62031f0155
Czarek Grudniewski <cgrudniewski@gmail.com>
2015-05-14 23:11:31 +0200

    test

--- src/test/java/pl/test/allegro/AllegroTest.java
+++ src/test/java/pl/test/allegro/AllegroTest.java
@@ -1,6 +1,8 @@
 package pl.test.allegro;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -16,6 +18,8 @@ public class AllegroTest {
 
     @BeforeMethod
     public void setUp() throws Exception {
+        driver = new FirefoxDriver(FirefoxDriverConf.getFirefoxProfileConfiguration());
+        PageFactory.initElements(driver, this);
     }
 
     @Test
@@ -24,5 +28,6 @@ public class AllegroTest {
 
     @AfterMethod
     public void tearDown() throws Exception {
+        driver.quit();
     }
 }
