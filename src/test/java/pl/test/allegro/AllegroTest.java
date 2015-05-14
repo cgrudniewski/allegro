42d9e9bc4f6c756b0d4a5556ab9f3a25e2908ea6
Czarek Grudniewski <cgrudniewski@gmail.com>
2015-05-14 22:38:55 +0200

    new class for tests AllegroTest

---
+++ src/test/java/pl/test/allegro/AllegroTest.java
@@ -0,0 +1,12 @@
+package pl.test.allegro;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Czarek on 2015-05-14.
+ */
+public class AllegroTest {
+
+    private WebDriver driver;
+    private String baseUrl = "http://allegro.pl";
+}
